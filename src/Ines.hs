e0dba3cf6c19d8b738535b01ae8ff5f36e3e216b
Alex Chavkin <alexlchavkin@gmail.com>
2022-09-03 17:49:09 -0400

    scaffolding for Extended Console Type / Vs System Type (unbuildable checkpoint)

--- src/Ines.hs
+++ src/Ines.hs
@@ -30,7 +30,6 @@ data InesHeader = InesHeader {
                   , chrramsize :: Word32 -- offset 11 CHR-RAM size
                   , chrnvramsize :: Word32 -- offset 11 CHR-NVRAM size
                   , timing     :: Timing -- offset 12 CPU/PPU timings
-                  , vssystem   :: Word8 -- offset 13 Vs. System Type or Extended Console Type
                   , miscroms   :: Word8 -- offset 14 Misc ROMs Present
                   , expdev     :: Word8 -- offset 15 Default Expansion Device
                   , battery    :: Bool -- Battery or non-volatile memory
@@ -39,6 +38,7 @@ data InesHeader = InesHeader {
                   , nes2       :: Bool -- NES2.0 extended format ROM?
                   , hwntm       :: HWNTM -- Hard-wried nametable mirroring type
                   , consoleType :: ConsoleType -- Regular NES or special?
+                  , vsSystemType :: VsSystemType -- VS System Type if VS System
                   } deriving Show
 
 type InesTrainer = ByteString
@@ -59,14 +59,14 @@ readInesHeader = do
     prgramshifts:
     chrramshifts:
     tmng:
-    vssystem:
+    b13:
     mscroms:
     expdv:
     _) <- replicateM 12 getWord8
   let prgromsize = word12PRG prgLSB prgchrMSBs
   let chrromsize = word12CHR chrLSB prgchrMSBs
   let (hwntm, battery, trainer512, hw4screenmode, m03) = parseFlags6 flags6
-  let (nes2, consoleType, m47) = parseFlags7 flags7
+  let (nes2, consoleType, m47) = parseFlags7 flags7 b13
   let (mapper, submapper) = mapperSubmapper m03 m47 mapperMSB
   let (prgramsize, prgnvramsize) = parseInesShifts prgramshifts
   let (chrramsize, chrnvramsize) = parseInesShifts chrramshifts
@@ -107,25 +107,44 @@ data HWNTM = HorizontalOrMapperControlled | Vertical deriving (Eq, Show)
 -- Header Byte 7 Flags
 type NES2_0 = Bool
 type MapperBits = Word8
-data ConsoleType = NESFamicom | VsSystem | Playchoice10 | ExtendedConsole deriving (Eq, Show)
+data ConsoleType = NESFamicom | VsSystem VSType | Playchoice10 | ExtendedConsole ECType deriving (Eq, Show)
+data ECType =   DecModeCPU -- Regular Famiclone, but with CPU that supports Decimal Mode
+             |  EPSM -- Regular NES/Famicom with EPSM module or plug-through cartridge
+             |  VT01  -- V.R. Technology VT01 with red/cyan STN palette
+             |  VT02  -- V.R. Technology VT02
+             |  VT03  -- V.R. Technology VT03
+             |  VT09  -- V.R. Technology VT09
+             |  VT32  -- V.R. Technology VT32
+             |  VT369 -- V.R. Technology VT369
+             |  UMCUM6578
+             |  FamicomNetworkSystem
+             deriving (Eq, Show) 
+
+data VSType = Placeholder deriving (Eq, Show)
 
 parseFlags6 :: Word8 -> (HWNTM, Battery, Trainer512B, HWFourScreenMode, MapperBits)
-parseFlags6 b = (ntm,ba,bt,sm4,m)
+parseFlags6 b b13 = (ntm,ba,bt,sm4,m)
   where ntm = if b .&. 0b1 /= 0 then Vertical else HorizontalOrMapperControlled
         ba = if b .&. 0b10 /= 0 then True else False
         bt = if b .&. 0b100 /= 0 then True else False
         sm4 = if b .&. 0b1000 /= 0 then True else False
         m = shiftR b 4
 
-parseFlags7 :: Word8 -> (NES2_0, ConsoleType, MapperBits)
-parseFlags7 b = (n,c,m)
+parseFlags7 :: Word8 -> Word8 -> (NES2_0, ConsoleType, MapperBits)
+parseFlags7 b b13 = (n,c,m)
   where n = b .&. 0b1100 == 0b1000
         c = case b .&. 0b11 of
               0b00 -> NESFamicom
-              0b01 -> VsSystem
+              0b01 -> VsSystem $ parseVST b13
               0b10 -> Playchoice10
-              0b11 -> ExtendedConsole
+              0b11 -> ExtendedConsole $ parseECT b13
         m = b .&. 0xF0
+
+parseECT :: Word8 -> ECType
+parseECT b = FamicomNetworkSystem --TODO
+
+parseVST :: Word8 -> VSType
+parseVST b = Placeholder -- TODO
 
 -- CPU/PPU Timing
 data Timing = RP2C02 | RP2C07 | Multiregion | UMC6527P deriving (Eq, Show)
