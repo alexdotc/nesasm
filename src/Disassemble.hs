17d82b665efeaa643c6f23f4e57deb4b05c6a52c
Alex Chavkin <alexlchavkin@gmail.com>
2022-09-26 00:03:46 -0400

    We're GETTIN SOMEWHERE (see below the cut)
    parseOpcode now has sig Word8 -> Maybe Opcode, returns Nothing
    where the machine would crash
    
    Successfully moving position and parsing our rom in StateT

--- src/Disassemble.hs
+++ src/Disassemble.hs
@@ -19,7 +19,7 @@ data DisasmState = DisasmState {
 type Disassembler = StateT DisasmState Maybe
 
 type InesROM = ByteString -- redundant, keep till reorg
-type Operand = Maybe ByteString
+type Operand = Maybe Word8
 type Instruction = (Mnemonic, Operand)
 
 indexMaybe :: ByteString -> Int -> Maybe Word8
@@ -27,21 +27,33 @@ indexMaybe b n
   | n >= B.length b = Nothing
   | otherwise = Just $ B.index b n
 
+isJumpOrBranch :: Mnemonic -> Bool
+isJumpOrBranch m = if elem m [JMP, JSR, BPL, BIT, BVC, RTS, BVS, BCC, BCS, BNE, BEQ] then True else False
+
 start :: Disassembler ()
 start = do
   opc <- next
-  (m, o) <- instruction
-  return ()
+  (m, o) <- instruction opc
+  case isJumpOrBranch m of -- placeholder
+    True -> return ()
+    False -> start
 
 next :: Disassembler Opcode
 next = do
   pos <- gets position
   rom <- gets prg
   updatePosition 1
-  lift $ parseOpcode <$> indexMaybe rom pos
+  lift $ indexMaybe rom pos >>= parseOpcode
 
-instruction :: Disassembler Instruction
-instruction = undefined
+instruction :: Opcode -> Disassembler Instruction
+instruction (Op m a) = do
+  pos <- gets position
+  rom <- gets prg
+  n <- case lenOpcode a of
+         0 -> return Nothing
+         1 -> (return $ indexMaybe rom pos) <* updatePosition 1
+         2 -> (return $ indexMaybe rom pos) <* updatePosition 2 -- TODO return 2 bytes not just 1
+  return (m, n)
 
 updatePosition :: Int -> Disassembler ()
 updatePosition n = modify $ \s -> s { position = position s + n }
@@ -49,5 +61,5 @@ updatePosition n = modify $ \s -> s { position = position s + n }
 disassemble :: InesROM -> Maybe DisasmState
 disassemble rom = do
   let initState = DisasmState 0 [] []
-  (_,d) <- runStateT start (initState rom)
+  d <- execStateT start (initState rom)
   return d
