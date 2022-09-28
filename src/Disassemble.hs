0126f8c371a298acece1b1f9255aaf5a83109687
Alex Chavkin <alexlchavkin@gmail.com>
2022-09-27 22:48:52 -0400

    Mark bytes we've disassembled to avoid infinite loops, follow branches

--- src/Disassemble.hs
+++ src/Disassemble.hs
@@ -3,6 +3,8 @@ module Disassemble (disassemble) where
 import Control.Monad (replicateM)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.State
+import Data.Array.Unboxed (UArray, (//), (!))
+import qualified Data.Array.Unboxed as UA
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import Data.Int (Int8)
@@ -12,8 +14,9 @@ import Opcode
 
 data DisasmState = DisasmState {
                      position :: Int
-                   , done :: [Instruction]
+                   , assembly :: [Instruction]
                    , stack :: [Word32]
+                   , done :: UArray Int Bool
                    , prg :: ByteString
                    } deriving Show
 
@@ -36,14 +39,23 @@ isBranch m = if elem m [BPL, BMI, BVC, BNE, BEQ, BCC, BCS, BVS] then True else F
 
 start :: Disassembler ()
 start = do
-  opc <- next
+  checkDone
+  opc <- opcode
   (m, o) <- instruction opc
-  modify $ \s -> s { done = (m, o) : done s }
+  modify $ \s -> s { assembly = (m, o) : assembly s }
   case isJump m of
     True -> return () -- placeholder
     False -> case isBranch m of
       True -> branch o >> start
       False -> start
+
+checkDone :: Disassembler ()
+checkDone = do
+  pos <- gets position
+  d <- gets done
+  case (d ! pos) of
+    True -> updatePosition 1 >> checkDone
+    False -> return ()
 
 branch :: Operand -> Disassembler ()
 branch (Just (o:_)) = updatePosition $ fromIntegral (fromIntegral o :: Int8) -- signed 8 bit value
@@ -51,10 +63,11 @@ branch (Just (o:_)) = updatePosition $ fromIntegral (fromIntegral o :: Int8) -- 
 subroutine :: Disassembler ()
 subroutine = undefined
 
-next :: Disassembler Opcode
-next = do
+opcode :: Disassembler Opcode
+opcode = do
   pos <- gets position
   rom <- gets prg
+  markDone
   updatePosition 1
   lift $ indexMaybe rom pos >>= parseOpcode
 
@@ -62,7 +75,7 @@ takeByte :: Disassembler Word8
 takeByte = do
   pos <- gets position
   rom <- gets prg
-  (lift $ indexMaybe rom pos) <* updatePosition 1
+  (lift $ indexMaybe rom pos) <* markDone <* updatePosition 1
 
 instruction :: Opcode -> Disassembler Instruction
 instruction (Op m a) = do
@@ -72,11 +85,14 @@ instruction (Op m a) = do
          TwoOpr -> return <$> replicateM 2 takeByte
   return (m, n)
 
+markDone :: Disassembler ()
+markDone = modify $ \s -> s { done = done s // [(position s, True)] }
+
 updatePosition :: Int -> Disassembler ()
 updatePosition n = modify $ \s -> s { position = position s + n }
 
 disassemble :: InesROM -> Maybe DisasmState
 disassemble rom = do
-  let initState = DisasmState 0 [] []
+  let initState = DisasmState 0 [] [] $ UA.array (0, B.length rom - 1) [(x,False) | x <- [0..B.length rom - 1]]
   d <- execStateT start (initState rom)
   return d
