e6f41337c8b116b9db87d03cc91e3ca2d3540bd3
Alex Chavkin <alexlchavkin@gmail.com>
2022-09-29 23:45:00 -0400

    add JMP , enable some basic trace debugging for now

--- src/Disassemble.hs
+++ src/Disassemble.hs
@@ -1,15 +1,18 @@
 module Disassemble (disassemble) where
 
-import Control.Monad (replicateM)
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.State (StateT, gets, execStateT, modify)
-import Data.Array.Unboxed (UArray, (//), (!))
+import           Control.Monad (replicateM)
+import           Control.Monad.Trans.Class (lift)
+import           Control.Monad.Trans.State (StateT, gets, execStateT, modify)
+import           Data.Array.Unboxed (UArray, (//), (!))
 import qualified Data.Array.Unboxed as UA
-import Data.Bits ((.|.), shiftL)
-import Data.ByteString (ByteString)
+import           Data.Bits ((.|.), shiftL)
+import           Data.ByteString (ByteString)
 import qualified Data.ByteString as B
-import Data.Int (Int8)
-import Data.Word (Word8, Word16)
+import           Data.Int (Int8)
+import           Data.Word (Word8, Word16)
+
+import Debug.Trace (trace)
+import Text.Printf
 
 import Opcode
 
@@ -25,7 +28,7 @@ type Disassembler = StateT DisasmState Maybe
 
 type InesROM = ByteString -- redundant, keep till reorg
 type Operand = Maybe [Word8]
-type Instruction = (Mnemonic, Operand)
+type Instruction = (Mnemonic, AddressingMode, Operand)
 
 indexMaybe :: ByteString -> Int -> Maybe Word8
 indexMaybe b n
@@ -39,15 +42,16 @@ start :: Disassembler ()
 start = do
   checkDone
   opc <- opcode
-  (m, o) <- instruction opc
-  modify $ \s -> s { assembly = (m, o) : assembly s }
+  (m, a, o) <- instruction opc
+  pos <- gets position
+  trace ("Now disassembling... " ++ show (m,a,o) ++ (printf "at Position %x" pos)) (modify $ \s -> s { assembly = (m, a, o) : assembly s })
   case isBranch m of
     True -> branch o >> start
     False -> case m of
-      JMP -> return () -- handle 2 types of jmp
+      JMP -> jmp (m, a, o) >> start
       RTI -> return () -- placeholder
-      JSR -> pushSubroutine o >> start
-      RTS -> popSubroutine >> start
+      JSR -> jsr o >> start
+      RTS -> rts >> start
       _   -> start
 
 checkDone :: Disassembler ()
@@ -61,21 +65,39 @@ checkDone = do
 branch :: Operand -> Disassembler ()
 branch (Just (o:_)) = updatePosition $ fromIntegral (fromIntegral o :: Int8) -- signed 8 bit value
 
-fuseAbsoluteOperand :: Operand -> Word16
-fuseAbsoluteOperand (Just (lsb:msb:[])) = (shiftL (fromIntegral msb :: Word16) 8) .|. (fromIntegral lsb :: Word16)
+fuseAbsoluteOperand :: [Word8] -> Word16
+fuseAbsoluteOperand (lsb:msb:[]) = (shiftL (fromIntegral msb :: Word16) 8) .|. (fromIntegral lsb :: Word16)
 
-pushSubroutine :: Operand -> Disassembler ()
-pushSubroutine o = do
+jsr :: Operand -> Disassembler ()
+jsr (Just o) = do
   let t = fuseAbsoluteOperand o
   modify $ \s -> s { stack = (fromIntegral (position s) :: Word16):(stack s) 
-                              , position = fromIntegral $ t - 0x8000 } -- maybe? TODO
+                              , position = fromIntegral $ t - 0x8000 }
 
-popSubroutine :: Disassembler ()
-popSubroutine = do
+rts :: Disassembler ()
+rts = do
   (top:rest) <- gets stack
   modify $ \s -> s { stack = rest, position = fromIntegral top }
-  
 
+jmp :: Instruction -> Disassembler ()
+jmp (_, a, o) = do
+  case a of
+    Indirect -> fail "Indirect jump"
+    Absolute -> jmpAbsolute o
+
+jmpIndirect :: Operand -> Disassembler ()
+jmpIndirect (Just o) = do
+  let pointer = fuseAbsoluteOperand o
+  modify $ \s -> s { position = fromIntegral $ pointer - 0x8000 }
+  value <- replicateM 2 takeByte
+  let target = fuseAbsoluteOperand value
+  modify $ \s -> s { position = fromIntegral $ target - 0x8000 }
+
+jmpAbsolute :: Operand -> Disassembler ()
+jmpAbsolute (Just o) = do
+  let t = fuseAbsoluteOperand o
+  modify $ \s -> s { position = fromIntegral $ t - 0x8000 }
+  
 opcode :: Disassembler Opcode
 opcode = do
   pos <- gets position
@@ -96,7 +118,7 @@ instruction (Op m a) = do
          ZeroOpr -> return Nothing
          OneOpr -> return <$> replicateM 1 takeByte
          TwoOpr -> return <$> replicateM 2 takeByte
-  return (m, n)
+  return (m, a, n)
 
 markDone :: Disassembler ()
 markDone = modify $ \s -> s { done = done s // [(position s, True)] }
